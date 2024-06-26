9b812abf95c84437df153949856f7c67be6119c8
Alex Nelson <alexander.nelson@nist.gov>
2024-06-26 17:39:41 -0400

    Update NIST disclaimer
    No effects were observed on Make-managed files.
    
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- dfxml_to_case.py
+++ dfxml_to_case.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
+# Portions of this file contributed by NIST are governed by the
+# following statement:
+#
 # This software was developed at the National Institute of Standards
 # and Technology by employees of the Federal Government in the course
-# of their official duties. Pursuant to title 17 Section 105 of the
-# United States Code this software is not subject to copyright
-# protection and is in the public domain. NIST assumes no
-# responsibility whatsoever for its use by other parties, and makes
-# no guarantees, expressed or implied, about its quality,
-# reliability, or any other characteristic.
+# of their official duties. Pursuant to Title 17 Section 105 of the
+# United States Code, this software is not subject to copyright
+# protection within the United States. NIST assumes no responsibility
+# whatsoever for its use by other parties, and makes no guarantees,
+# expressed or implied, about its quality, reliability, or any other
+# characteristic.
 #
 # We would appreciate acknowledgement if the software is used.
 
@@ -16,7 +19,7 @@ This program is based on example data from:
 https://github.com/casework/case/blob/master/examples/file.json
 """
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 import logging
 import os
